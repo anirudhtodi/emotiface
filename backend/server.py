c28f82bc930b2fbaf70f09653e731b024fd83593
joshuaevenson <joshuaevenson@gmail.com>
2011-12-02 10:26:22 -0800

    848

--- backend/server.py
+++ backend/server.py
@@ -12,7 +12,7 @@ class Server(Resource, threading.Thread):
     """
     
     """
-    max_payload_size = 448
+    max_payload_size = 848
     
     def __init__(self):
         threading.Thread.__init__(self)
