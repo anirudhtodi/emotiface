eb9630c96fa8cff3a2a3bdc550aaf5c8c755a183
Peter Cottle <petermcottle@gmail.com>
2011-12-01 22:07:37 -0800

    fixed port

--- backend/static/emotiface.user.js
+++ backend/static/emotiface.user.js
@@ -20,9 +20,9 @@ _my_style.type='text/css';
 _my_style.rel="stylesheet";
 _my_style.media="all";
 
-_my_script1.src='http://127.0.0.1/static/thejs.js';
+_my_script1.src='http://127.0.0.1:7049/static/thejs.js';
 _my_script3.src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js';
-_my_style.href='http://127.0.0.1/static/style.css';
+_my_style.href='http://127.0.0.1:7049/static/style.css';
 
 document.getElementsByTagName('head')[0].appendChild(_my_script1);
 document.getElementsByTagName('head')[0].appendChild(_my_script3);
