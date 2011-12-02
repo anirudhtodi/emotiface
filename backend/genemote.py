286c85081f4ce9e2676c3d4347cf5e487a285159
joshuaevenson <joshuaevenson@gmail.com>
2011-12-01 20:21:10 -0800

    large fixes to the server, it is alrgery working except for decoding

--- backend/genemote.py
+++ backend/genemote.py
@@ -1,3 +1,22 @@
-import os
+import subprocess, os, time, shutil
 
-os.system("osascript gen.sc")
+
+
+
+def foo():
+    try:
+        path = os.path.expanduser("~/")
+        path = os.path.join(path, "Movies/Movie Recording.mov")
+        print path
+        shutil.move(path, "./movie.mov")
+    except Exception as e:
+        print "EXCEPTIOPM: %s" % e
+        time.sleep(5)
+        foo()
+
+subprocess.check_call(["osascript", "gen.sc"])
+foo()
+
+#import os
+#os.system("osascript gen.sc")
+#os.system("mv ~/Movies/Movie\ Recording.mov ./movie.mov")
